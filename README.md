7eae97d44ebfc520f809852d0a1429ab255536e1
Yangguang Li <liyangguangcn@gmail.com>
2023-03-26 16:26:14 -0700

    Init basic structure.

---
+++ README.md
@@ -0,0 +1,3 @@
+# ChatGPT search engine
+
+A very simple extension to allow you to set ChatGPT as your Chrome's default search engine.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import {onMount} from 'svelte';
+  let query = '';
+
+  onMount(() => {
+    query = new URLSearchParams(window.location.search).get('q') || '';
+  })
+
+  function search() {
+    console.log('searching', query);
+    query = '';
+  }
+</script>
+
+<header>
+  <h1>ChatGPT search engine</h1>
+</header>
+
+<main>
+  <form on:submit|preventDefault={search}>
+    <input type="text" bind:value={query}>
+  </form>
+</main>
+
+<footer>
+  <p>Developed by <a href="https://liyangguang.com" target="_blank">Yangguang Li</a>.</p>
+</footer>
+
+<style lang="scss">
+main {
+  padding: 2em;
+}
+</style>
